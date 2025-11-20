78c6b21a2728041e95a29c71d31e6add2bb09b8f
nikhisable5050 <nikhilsable5050@gmail.com>
2025-11-20 14:42:42 +0530

    Refactor App.jsx layout and clean component structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,27 @@
 
 import Navbar from "./components/Navbar";
+import Home from "./sections/Home";
+import About from "./sections/About";
+import Skills from "./sections/Skills";
+import Projects from "./sections/Projects";
+import Experience from "./sections/Experience";
+import Testimonials from "./sections/Testimonials";
+import Contact from "./sections/Contact";
+import Footer from "./sections/Footer";
 
 export default function App() {
   return (
     <div>
       <Navbar />
-    </div>
+      <Home />
+      <About />
+      <Skills />
+      <Projects />
+      <Experience />
+      <Testimonials />
+      <Contact />
+      <Footer />
+
+      </div>
   )
 }
--- src/sections/Projects.jsx
+++ src/sections/Projects.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Project = () => {
   return (
     <div className="w-full h-screen">
-      Home
+      Projects
     </div>
   )
 }
