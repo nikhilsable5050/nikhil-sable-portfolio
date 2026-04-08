611b3f60143b18e4024a9d81099b833c2b5916d8
nikhilsable5050 <nikhilsable5050@gmail.com>
2026-04-08 18:45:27 +0530

    fix controller endpoints

--- src/main/java/com/nikhisable/portfolio/controller/HomeController.java
+++ src/main/java/com/nikhisable/portfolio/controller/HomeController.java
@@ -1,13 +1,13 @@
 package com.nikhisable.portfolio.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
-@RestController
+@Controller
 public class HomeController {
 
-    @GetMapping("/")
-    public String home() {
-        return "Backend is Live 🚀";
+    @RequestMapping({"/", "", "/home"})
+    public String showHomePage() {
+        return "home";
     }
 }
